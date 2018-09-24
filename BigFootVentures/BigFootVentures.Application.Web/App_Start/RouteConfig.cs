49406883419e9005e0cab4c625fc8fb9027b48ae
Angelo Tupaz <blackisle_12@yahoo.com>
2018-09-24 23:04:34 +0800

    update routing

--- BigFootVentures/BigFootVentures.Application.Web/App_Start/RouteConfig.cs
+++ BigFootVentures/BigFootVentures.Application.Web/App_Start/RouteConfig.cs
@@ -8,12 +8,7 @@ namespace BigFootVentures.Application.Web
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            routes.MapMvcAttributeRoutes(); //Enables Attribute Routing
         }
     }
 }
--- BigFootVentures/BigFootVentures.Application.Web/Controllers/HomeController.cs
+++ BigFootVentures/BigFootVentures.Application.Web/Controllers/HomeController.cs
@@ -7,6 +7,7 @@ using static BigFootVentures.Application.Web.Models.VMEnums;
 
 namespace BigFootVentures.Application.Web.Controllers
 {
+    [RoutePrefix("Home")]
     public class HomeController : Controller
     {
         #region "Private Members"
@@ -26,13 +27,21 @@ namespace BigFootVentures.Application.Web.Controllers
 
         #endregion
 
-        #region "Views"
+        #region "Default"
 
+        [Route("~/")]
+        [Route("")]
+        [Route("Index")]
         public ActionResult Index()
         {
             return View();
         }
 
+        #endregion
+
+        #region "Brands"
+
+        [Route("Brands/{startIndex?}/{rowCount?}/{page?}", Name = "Brands")]
         public ActionResult Brands(int startIndex = 0, int rowCount = 10, int page = 1)
         {
             startIndex = (page - 1) * rowCount;
@@ -56,67 +65,64 @@ namespace BigFootVentures.Application.Web.Controllers
             return View(pageResult);
         }
 
-        public ActionResult Brand(int ID, int? toEdit)
+        [Route("Brand/{ID:int}", Name = "BrandView")]
+        public ActionResult Brand(int ID)
         {
-            var brand = ID > 0 ? this._managementBrandService.Get(ID) : new Brand();
+            var brand = this._managementBrandService.Get(ID);
             var model = new VMModel<Brand>
             {
                 Record = brand,
-                PageMode = toEdit != null || ID == 0 ? PageMode.Edit : PageMode.View,
+                PageMode = PageMode.View
             };
 
-            if (TempData.ContainsKey("IsNewlyCreated"))
+            if (TempData.ContainsKey("IsBrandPosted"))
             {
-                model.PageMode = PageMode.PersistSuccess;
-                TempData.Remove("IsNewlyCreated");
+                model.PageMode = (bool)TempData["IsBrandPosted"] ? PageMode.PersistSuccess : PageMode.PersistError;
+                TempData.Remove("IsBrandPosted");
             }
 
-            return View(model);
+            return View("Brand", model);
         }
 
-        public ActionResult Companies(int startIndex = 0, int rowCount = 10, int page = 1)
+        [Route("Brand/Edit/{ID:int}", Name = "BrandEdit")]
+        public ActionResult BrandEdit(int ID)
         {
-            startIndex = (page - 1) * rowCount;
-
-            var companies = this._managementCompanyService.Get(startIndex, rowCount, out int total);
-            var pageResult = new VMPageResult<Company>
+            var brand = this._managementBrandService.Get(ID);
+            var model = new VMModel<Brand>
             {
-                StartIndex = startIndex,
-                RowCount = rowCount,
-                Page = page,
-                Total = total,
-                Records = companies
+                Record = brand,
+                PageMode = PageMode.Edit
             };
 
-            if (TempData.ContainsKey("IsRedirectFromDelete"))
-            {
-                pageResult.IsRedirectFromDelete = true;
-                TempData.Remove("IsRedirectFromDelete");
-            }
-
-            return View(pageResult);         
+            return View("Brand", model);
         }
 
-        #endregion
+        [Route("Brand/New", Name = "BrandNew")]
+        public ActionResult BrandNew()
+        {            
+            var model = new VMModel<Brand>
+            {
+                Record = new Brand(),
+                PageMode = PageMode.Edit
+            };
 
-        #region "Http Methods"
+            return View("Brand", model);
+        }
 
         [HttpPost]
+        [Route("Brand", Name = "BrandPost")]
         public ActionResult Brand(VMModel<Brand> model)
         {
             try
             {
                 if (ModelState.IsValid)
                 {
-                    if (model.Record.CategoriesSelected != null)                    
+                    if (model.Record.CategoriesSelected != null)
                         model.Record.Category = string.Join(";", model.Record.CategoriesSelected);
 
                     if (model.Record.ID == 0)
                     {
                         this._managementBrandService.Insert(model.Record);
-
-                        TempData.Add("IsNewlyCreated", true);
-                        return RedirectToAction("Brand", "Home", new { model.Record.ID });
                     }
                     else
                     {
@@ -124,18 +130,19 @@ namespace BigFootVentures.Application.Web.Controllers
                     }
                 }
 
-                model.PageMode = PageMode.PersistSuccess;
+                TempData.Add("IsBrandPosted", true);
             }
             catch (Exception ex)
             {
-                //log exception here     
-                model.PageMode = PageMode.PersistError;
+                //log exception here                
+                TempData.Add("IsBrandPosted", false);
             }
-
-            return View(model);
+            
+            return RedirectToRoute("BrandView", new { ID = model.Record.ID });
         }
 
         [HttpGet]
+        [Route("Brand/Delete/{ID:int}", Name = "BrandDelete")]
         public ActionResult BrandDelete(int ID)
         {
             try
@@ -146,10 +153,20 @@ namespace BigFootVentures.Application.Web.Controllers
             }
             catch (Exception ex)
             {
-                //log exception here                     
+                //log exception here
             }
 
             return RedirectToAction("Brands", "Home");
+        }
+
+        #endregion
+
+        #region "Companies"
+
+        [Route("Company/New/SelectRecordType", Name = "CompanyNewSelectRecordType")]
+        public ActionResult CompanyNewSelectRecordType()
+        {
+            return View();
         }
 
         #endregion
