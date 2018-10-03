9e3d34232c4b917c065ff74651ab7882b94bd9bd
Angelo Tupaz <blackisle_12@yahoo.com>
2018-10-03 14:30:14 +0800

    rearrange components

---
+++ BigFootVentures/BigFootVentures.Application.Web/Models/ViewModels/VMAssociation.cs
@@ -0,0 +1,15 @@
+﻿namespace BigFootVentures.Application.Web.Models.ViewModels
+{
+    public sealed class VMAssociation
+    {
+        #region "Properties"
+
+        public string ParentName { get; set; }
+
+        public string ChildName { get; set; }
+        public string ChildNameContainer { get; set; }
+        public string ChildIDContainer { get; set; }
+
+        #endregion
+    }
+}
