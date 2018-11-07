bf87baa8a2f4bdc60337e86a9b67e42881e5cfca
alphonso.velarde <alphonso.velarde@essilor.com>
2018-11-07 22:31:29 +0800

    new nav

--- BigFootVentures/BigFootVentures.Application.Web/Scripts/nav-scripts.js
+++ BigFootVentures/BigFootVentures.Application.Web/Scripts/nav-scripts.js
@@ -1,13 +1,13 @@
 ﻿$(function () {
     $('[name="nav-toggle"]').on('click', function () {
-        $('#sidebar, #content').toggleClass('active');
+        $('[name="sidebar"]').toggleClass('active');
         $('[name="overlay"]').toggleClass('active');
         $('.collapse.in').toggleClass('in');
         $('a[aria-expanded=true]').attr('aria-expanded', 'false');
     });
 
     $('[name="overlay"]').on('click', function () {
-        $('#sidebar, #content').toggleClass('active');
+        $('#sidebar').toggleClass('active');
         $('[name="overlay"]').toggleClass('active');
     });
 }); 
