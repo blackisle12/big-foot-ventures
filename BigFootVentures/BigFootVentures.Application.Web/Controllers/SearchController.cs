745865185ec0eb4c976a27441ab12949a33e511f
Angelo Tupaz <blackisle_12@yahoo.com>
2018-12-24 23:09:19 +0800

    add changes on Search controller, and some layout

--- BigFootVentures/BigFootVentures.Application.Web/Controllers/SearchController.cs
+++ BigFootVentures/BigFootVentures.Application.Web/Controllers/SearchController.cs
@@ -1,7 +1,6 @@
 ﻿using BigFootVentures.Application.Web.Models.ViewModels;
-using BigFootVentures.Business.Objects.Wrapper;
+using BigFootVentures.Business.Objects.Management;
 using BigFootVentures.Service.BusinessService;
-using System.Collections.Generic;
 using System.Web.Mvc;
 
 namespace BigFootVentures.Application.Web.Controllers
@@ -14,13 +13,21 @@ namespace BigFootVentures.Application.Web.Controllers
 
         private readonly ISearchService _searchService = null;
 
+        private readonly IManagementService<AgreementT> _managementAgreementService = null;
+        private readonly IManagementService<Brand> _managementBrandService = null;
+
         #endregion
 
         #region "Constructors"
 
-        public SearchController(ISearchService searchService)
+        public SearchController(ISearchService searchService,
+            IManagementService<AgreementT> managementAgreementService,
+            IManagementService<Brand> managementBrandService)
         {
             this._searchService = searchService;
+
+            this._managementAgreementService = managementAgreementService;
+            this._managementBrandService = managementBrandService;
         }
 
         #endregion
@@ -39,6 +46,66 @@ namespace BigFootVentures.Application.Web.Controllers
             ViewBag.Keyword = keyword;
 
             return View(searchResult);
+        }
+
+        [Route("Agreement/{rowCount?}/{page?}/{keyword?}", Name = "SearchAgreement")]
+        public ActionResult Agreement(int rowCount = 25, int page = 1, string keyword = "")
+        {
+            var searchResultObject = new VMSearchResultObject<AgreementT>();
+            var startIndex = (page - 1) * rowCount;
+            var agreements = string.IsNullOrWhiteSpace(keyword) ?
+                this._managementAgreementService.Get(startIndex, rowCount, out int total) :
+                this._managementAgreementService.GetByKeyword(keyword, startIndex, rowCount, out total);
+
+            searchResultObject.ObjectResult = new VMPageResult<AgreementT>
+            {
+                StartIndex = startIndex,
+                RowCount = rowCount,
+                Page = page,
+                Total = total,
+                Records = agreements
+            };
+
+            var searchResultWrapperList = this._searchService.Search(keyword);
+
+            searchResultObject.SearchResult = new VMSearchResult
+            {
+                Table = searchResultWrapperList
+            };
+
+            ViewBag.Keyword = keyword;
+
+            return View(searchResultObject);
+        }
+
+        [Route("Brand/{rowCount?}/{page?}/{keyword?}", Name = "SearchBrand")]
+        public ActionResult Brand(int rowCount = 25, int page = 1, string keyword = "")
+        {
+            var searchResultObject = new VMSearchResultObject<Brand>();
+            var startIndex = (page - 1) * rowCount;
+            var brands = string.IsNullOrWhiteSpace(keyword) ?
+                this._managementBrandService.Get(startIndex, rowCount, out int total) :
+                this._managementBrandService.GetByKeyword(keyword, startIndex, rowCount, out total);
+
+            searchResultObject.ObjectResult = new VMPageResult<Brand>
+            {
+                StartIndex = startIndex,
+                RowCount = rowCount,
+                Page = page,
+                Total = total,
+                Records = brands
+            };
+
+            var searchResultWrapperList = this._searchService.Search(keyword);
+
+            searchResultObject.SearchResult = new VMSearchResult
+            {
+                Table = searchResultWrapperList
+            };
+
+            ViewBag.Keyword = keyword;
+
+            return View(searchResultObject);
         }
 
         #endregion
---
+++ BigFootVentures/BigFootVentures.Application.Web/Models/ViewModels/VMSearchResultObject.cs
@@ -0,0 +1,15 @@
+﻿using BigFootVentures.Business.Objects;
+
+namespace BigFootVentures.Application.Web.Models.ViewModels
+{
+    public sealed class VMSearchResultObject<TModel> where TModel : BusinessBase
+    {
+        #region "Properties"
+
+        public VMSearchResult SearchResult { get; set; }
+
+        public VMPageResult<TModel> ObjectResult { get; set; }
+
+        #endregion
+    }
+}
