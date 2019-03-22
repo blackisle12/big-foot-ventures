16ca9c5369a59335d0d1370af89876a4b17f1f14
Angelo Tupaz <blackisle_12@yahoo.com>
2019-03-22 17:29:03 +0800

    change password page

--- BigFootVentures/BigFootVentures.Application.Web/Controllers/AccountController.cs
+++ BigFootVentures/BigFootVentures.Application.Web/Controllers/AccountController.cs
@@ -1,7 +1,9 @@
-﻿using BigFootVentures.Application.Web.Models.Utilities;
+﻿using BigFootVentures.Application.Web.Models.Security;
+using BigFootVentures.Application.Web.Models.Utilities;
 using BigFootVentures.Application.Web.Models.ViewModels;
 using BigFootVentures.Business.Objects.Management;
 using BigFootVentures.Service.BusinessService;
+using System;
 using System.Web.Mvc;
 using static BigFootVentures.Application.Web.Models.VMEnums;
 
@@ -46,6 +48,49 @@ namespace BigFootVentures.Application.Web.Controllers
             };
 
             return View("AccountInformationView", model);
+        }
+
+        [Route("ChangePassword", Name = "ChangePassword")]
+        public ActionResult ChangePassword()
+        {
+            return View("ChangePassword");
+        }
+
+        [HttpPost]
+        [Route("ChangePasswordPost", Name = "ChangePasswordPost")]
+        public ActionResult ChangePasswordPost(string currentPassword, string newPassword, string confirmPassword)
+        {
+            bool isSuccess = true;
+
+            try
+            {
+                if (string.Equals(currentPassword, newPassword))
+                    throw new Exception("New password should not be the same as current password.");
+
+                if (!string.Equals(newPassword, confirmPassword))
+                    throw new Exception("Password does not matched.");
+
+                var session = SessionUtils.GetUserAccount();
+                var account = this._service.Get(session.ID);
+
+                if (!string.Equals(currentPassword, PasswordEncryption.Decrypt(account.Password)))
+                    throw new Exception("Current password is incorrect.");
+
+                account.Password = PasswordEncryption.Encrypt(newPassword);
+
+                this._service.UpdateUserAccount(account);
+            }
+            catch (Exception ex)
+            {
+                TempData.Add("ErrorMessage", ex.Message);
+                isSuccess = false;
+            }
+            finally
+            {
+                TempData.Add("IsSuccess", isSuccess);
+            }
+            
+            return RedirectToRoute("ChangePassword");
         }
 
         #endregion
