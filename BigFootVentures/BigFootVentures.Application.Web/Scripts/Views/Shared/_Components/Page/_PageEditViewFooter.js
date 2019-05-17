79badeef533fd0a0035c43d21da7a27c0b3f50c9
Angelo Tupaz <blackisle_12@yahoo.com>
2019-05-17 19:29:28 +0800

    some fixes

--- BigFootVentures/BigFootVentures.Application.Web/Scripts/Views/Shared/_Components/Page/_PageEditViewFooter.js
+++ BigFootVentures/BigFootVentures.Application.Web/Scripts/Views/Shared/_Components/Page/_PageEditViewFooter.js
@@ -3,3 +3,17 @@
         window.location.href = url;
     }
 }
+
+$(function () {
+    $('.open-link').click(function () {
+        var link = $(this).data('link');
+
+        if (!link.startsWith('http')) {
+            link = 'http://' + link;
+        }
+
+        var win = window.open(link, '_blank');
+
+        win.focus();
+    });
+});
